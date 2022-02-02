569433c85b3d2e428b6d6bc5239254c2f01b1ca7
l4zs <business@l4zs.de>
2022-02-02 18:18:07 +0100

    update userdev version, use plugin-yml to generate the plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("io.papermc.paperweight.userdev") version "1.3.4-SNAPSHOT"
+    id("io.papermc.paperweight.userdev") version "1.3.4"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
 }
 
 group = "org.example"
@@ -30,4 +31,22 @@ tasks {
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
+}
+
+bukkit {
+    name = "ExamplePlugin"
+    apiVersion = "1.18"
+    authors = listOf(
+        "Your Name",
+    )
+    main = "$group.exampleplugin.ExamplePlugin"
+    version = getVersion().toString()
+    libraries = listOf(
+        "net.axay:kspigot:1.18.0",
+        "org.jetbrains.kotlin:kotlin-stdlib:1.6.10",
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0",
+        "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0",
+        "com.mojang:brigadier:1.0.18",
+        "com.google.code.gson:gson:2.8.9",
+    )
 }

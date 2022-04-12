036d49554dd1eedfd9671d8d1eb225861cd606b5
mooziii <63669478+mooziii@users.noreply.github.com>
2022-04-12 22:25:57 +0200

    Update ExamplePlugin.kt

--- src/main/kotlin/org/example/exampleplugin/ExamplePlugin.kt
+++ src/main/kotlin/org/example/exampleplugin/ExamplePlugin.kt
@@ -1,15 +1,14 @@
 package org.example.exampleplugin
 
-import org.bukkit.ChatColor
 import net.axay.kspigot.main.KSpigot
 
 class ExamplePlugin : KSpigot() {
 
     override fun startup() {
-        logger.info("${ChatColor.GREEN}The Plugin was enabled!")
+        logger.info("The Plugin was enabled!")
     }
 
     override fun shutdown() {
-        logger.info("${ChatColor.RED}The Plugin was disabled!")
+        logger.info("The Plugin was disabled!")
     }
 }

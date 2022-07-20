7fc07d53b93b3babca8bdaa97b514a5d7415d50d
l4zs <business@l4zs.de>
2022-07-20 01:16:38 +0100

    bump versions and add load() example

--- src/main/kotlin/org/example/exampleplugin/ExamplePlugin.kt
+++ src/main/kotlin/org/example/exampleplugin/ExamplePlugin.kt
@@ -4,6 +4,10 @@ import net.axay.kspigot.main.KSpigot
 
 class ExamplePlugin : KSpigot() {
 
+    override fun load() {
+        logger.info("The Plugin was loaded!")
+    }
+
     override fun startup() {
         logger.info("The Plugin was enabled!")
     }
